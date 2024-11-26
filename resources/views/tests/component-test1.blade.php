3dd5cf607d9dbbde962f47260e8c8bf04a74fae4
YuiHirayama <weipingshan87@gmail.com>
2024-11-26 17:39:59 +0900

    classBase

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -7,6 +7,6 @@
 @vite('resources/css/app.css')
 
 {{-- Component 属性でデータ受け渡し(@props) --}}
-<x-card title="タイトル1" content="本文1" :message="$message" />
-<x-card title="タイトル2" />
-<x-card title="CSSを変更したい" class="bg-red-300" />
+<x-tests.card title="タイトル1" content="本文1" :message="$message" />
+<x-tests.card title="タイトル2" />
+<x-tests.card title="CSSを変更したい" class="bg-red-300" />
