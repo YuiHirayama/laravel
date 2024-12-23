01a23d79ce297a4deb6da96b6f64f1a6dc03487a
YuiHirayama <weipingshan87@gmail.com>
2024-12-23 22:59:44 +0900

    softDelete

--- resources/views/components/flash-messeage.blade.php
+++ resources/views/components/flash-messeage.blade.php
@@ -2,8 +2,8 @@
 
 {{-- create画面_statusを渡してbgcolor変更 --}}
 @php
-    if($status === 'info'){ $bgcolor = 'bg-blue-300';}
-    if($status === 'error'){ $bgcolor = 'bg-red-500';}
+    if(session('status') === 'info'){ $bgcolor = 'bg-blue-300';}
+    if(session('status') === 'alert'){ $bgcolor = 'bg-red-500';}
 @endphp
 
 @if (session('message'))
