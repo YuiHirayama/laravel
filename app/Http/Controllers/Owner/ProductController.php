48552b0ad4a855115d76048b1e439dc875670900
YuiHirayama <weipingshan87@gmail.com>
2025-01-08 00:19:51 +0900

    product index,eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,15 +4,40 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
+use Illuminate\Support\Facades\Auth;
+use Closure;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function (Request $request, Closure $next) {
+
+            $id = $request->route()->parameter('product');
+            if(!is_null($id)){
+            $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId;
+                if($productId !== Auth::id()){
+                    abort(404);
+                }
+             }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id', Auth::id())->get();
+        
+        return view('owner.products.index',
+        compact('ownerInfo'));
     }
 
     /**
