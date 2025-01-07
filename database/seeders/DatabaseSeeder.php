8de8dc7d1ca461df8907057f95da65bf71ace2e1
YuiHirayama <weipingshan87@gmail.com>
2025-01-07 22:08:35 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
