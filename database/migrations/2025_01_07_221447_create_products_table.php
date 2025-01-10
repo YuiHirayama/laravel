d09019c8e4c16f9bed23cec148f8863225a18528
YuiHirayama <weipingshan87@gmail.com>
2025-01-10 23:49:58 +0900

    product create

--- database/migrations/2025_01_07_221447_create_products_table.php
+++ database/migrations/2025_01_07_221447_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
             ->constrained()
             ->onUpdate('cascade')
@@ -20,7 +26,20 @@ return new class extends Migration
             $table->foreignId('secondary_category_id')
             ->constrained();
             $table->foreignId('image1')
-            ->nullable('images');
+            ->nullable()
+            ->constrained('images');
+            $table->timestamps();
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->timestamps();
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->timestamps();
+            $table->foreignId('image4')
+            ->nullable()
+            ->constrained('images');
             $table->timestamps();
         });
     }
