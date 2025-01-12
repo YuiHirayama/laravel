1af92be28176fbcfe96067c49a350112485db21b
YuiHirayama <weipingshan87@gmail.com>
2025-01-12 15:32:26 +0900

    constant

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'Constant' => App\Constants\Common::class,
     ])->toArray(),
 
 ];
